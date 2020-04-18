02e475277ed881cb6d4f4e86699ad8362f4d6a52
Florian Langenhahn <florian.langenhahn@uberall.com>
2020-04-18 02:02:09 +0200

    inherit constructors of WorkerPoolImpl

--- src/main/groovy/grails/plugins/jesque/GrailsWorkerImpl.groovy
+++ src/main/groovy/grails/plugins/jesque/GrailsWorkerImpl.groovy
@@ -1,27 +1,16 @@
 package grails.plugins.jesque
 
+import groovy.transform.InheritConstructors
 import groovy.util.logging.Slf4j
-import net.greghaines.jesque.Config
 import net.greghaines.jesque.Job
-import net.greghaines.jesque.worker.JobFactory
-import net.greghaines.jesque.worker.NextQueueStrategy
 import net.greghaines.jesque.worker.WorkerAware
 import net.greghaines.jesque.worker.WorkerPoolImpl
-import redis.clients.jedis.Jedis
-import redis.clients.util.Pool
 
 import static net.greghaines.jesque.worker.WorkerEvent.JOB_EXECUTE
 
 @Slf4j
+@InheritConstructors
 class GrailsWorkerImpl extends WorkerPoolImpl {
-
-    GrailsWorkerImpl(final Config config,
-                     final Collection<String> queues,
-                     final JobFactory factory,
-                     final Pool<Jedis> jedisPool,
-                     final NextQueueStrategy nextQueueStrategy) {
-        super(config, queues, factory, jedisPool, nextQueueStrategy)
-    }
 
     @Override
     protected Object execute(final Job job, final String curQueue, final Object instance) throws Exception {
